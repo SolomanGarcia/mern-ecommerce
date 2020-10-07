2cd93c3bb2321c386252be21945320b41eb120ce
Soloman Garcia <soloman.garcia@gmail.com>
2020-10-07 09:03:13 -0600

    create navbar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer>
+      <Container>
+        <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
