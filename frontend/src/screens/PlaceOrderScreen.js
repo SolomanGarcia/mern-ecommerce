645d50b79c05cb2c3469fd41de7116089758eec6
Soloman Garcia <soloman.garcia@gmail.com>
2020-10-22 17:02:49 -0600

    fix shippping cost

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -20,7 +20,9 @@ const PlaceOrderScreen = ({ history }) => {
     cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   );
 
-  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
+  cart.shippingPrice = addDecimals(
+    cart.itemsPrice > 100 ? 0 : cart.itemsPrice * 0.05
+  );
 
   cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
 
