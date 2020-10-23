2167d8220a34a2eab80f2477e2df75ee412396d0
Soloman Garcia <soloman.garcia@gmail.com>
2020-10-22 19:50:07 -0600

    add more products

--- backend/data/products.js
+++ backend/data/products.js
@@ -32,8 +32,8 @@ const products = [
     category: "The Beatles",
     price: 119.99,
     countInStock: 5,
-    rating: 3,
-    numReviews: 12
+    rating: 4,
+    numReviews: 9
   },
   {
     name: "LegoArt Paul McCartney",
@@ -44,20 +44,104 @@ const products = [
     category: "The Beatles",
     price: 119.99,
     countInStock: 11,
-    rating: 5,
-    numReviews: 12
+    rating: 4.8,
+    numReviews: 15
   },
   {
-    name: "LegoArt The Beatles Collection",
-    image: "/images/beatles.png",
+    name: "LegoArt Darth Maul",
+    image: "/images/maul.jpg",
     description:
-      "Inspired by the iconic portraits included in the Beatles’ famous White Album, this new LEGO® building experience invites you to create one of four detailed wall art portraits of your favorite Beatle, whether it’s John Lennon, Paul McCartney, George Harrison or Ringo Starr. While building, immerse yourself in a Soundtrack of stories and unexpected details about the band.",
+      "Discover a new LEGO® experience as you create portraits of Star Wars™ legends. Based on brand-new artwork, Darth Maul™, Darth Vader™ and Kylo Ren™ are brought to life in three powerful colour portraits, each representing its own era in the Star Wars saga. Build along with a special Soundtrack to enhance your experience.",
     brand: "Lego",
-    category: "The Beatles",
-    price: 439.99,
+    category: "Star Wars",
+    price: 119.99,
+    countInStock: 0,
+    rating: 5,
+    numReviews: 6
+  },
+  {
+    name: "LegoArt Kylo Ren",
+    image: "/images/kylo.jpg",
+    description:
+      "Discover a new LEGO® experience as you create portraits of Star Wars™ legends. Based on brand-new artwork, Darth Maul™, Darth Vader™ and Kylo Ren™ are brought to life in three powerful colour portraits, each representing its own era in the Star Wars saga. Build along with a special Soundtrack to enhance your experience.",
+    brand: "Lego",
+    category: "Star Wars",
+    price: 119.99,
+    countInStock: 7,
+    rating: 4.2,
+    numReviews: 9
+  },
+  {
+    name: "LegoArt Darth Vader",
+    image: "/images/vader.jpg",
+    description:
+      "Discover a new LEGO® experience as you create portraits of Star Wars™ legends. Based on brand-new artwork, Darth Maul™, Darth Vader™ and Kylo Ren™ are brought to life in three powerful colour portraits, each representing its own era in the Star Wars saga. Build along with a special Soundtrack to enhance your experience.",
+    brand: "Lego",
+    category: "Star Wars",
+    price: 119.99,
+    countInStock: 4,
+    rating: 4.1,
+    numReviews: 13
+  },
+  {
+    name: "LegoArt Iron Man v1",
+    image: "/images/ironman1.jpg",
+    description:
+      "Find your strong suit. New LEGO® Art Marvel Studios Iron Man lets you display your passion like never before.",
+    brand: "Lego",
+    category: "Marvel Studios",
+    price: 119.99,
+    countInStock: 2,
+    rating: 4.3,
+    numReviews: 7
+  },
+  {
+    name: "LegoArt Iron Man v2",
+    image: "/images/ironman2.jpg",
+    description:
+      "Find your strong suit. New LEGO® Art Marvel Studios Iron Man lets you display your passion like never before.",
+    brand: "Lego",
+    category: "Marvel Studios",
+    price: 119.99,
+    countInStock: 10,
+    rating: 4.1,
+    numReviews: 5
+  },
+  {
+    name: "LegoArt Iron Man v3",
+    image: "/images/ironman3.jpg",
+    description:
+      "Find your strong suit. New LEGO® Art Marvel Studios Iron Man lets you display your passion like never before.",
+    brand: "Lego",
+    category: "Marvel Studios",
+    price: 119.99,
+    countInStock: 14,
+    rating: 3.9,
+    numReviews: 2
+  },
+  {
+    name: "LegoArt Marilyn Monroe v1",
+    image: "/images/marilyn1.jpg",
+    description:
+      "As you build, listen to the included Soundtrack and immerse yourself in a world of art and creativity as you hear unexpected details and audio stories linked to the set. Each set comes with four different building options. Purchase four sets to recreate Andy Warhol’s legendary Marilyn portrait series.",
+    brand: "Lego",
+    category: "Andy Warhol",
+    price: 119.99,
+    countInStock: 8,
+    rating: 3.7,
+    numReviews: 3
+  },
+  {
+    name: "LegoArt Marilyn Monroe v2",
+    image: "/images/marilyn2.jpg",
+    description:
+      "As you build, listen to the included Soundtrack and immerse yourself in a world of art and creativity as you hear unexpected details and audio stories linked to the set. Each set comes with four different building options. Purchase four sets to recreate Andy Warhol’s legendary Marilyn portrait series.",
+    brand: "Lego",
+    category: "Andy Warhol",
+    price: 119.99,
     countInStock: 7,
     rating: 3.5,
-    numReviews: 10
+    numReviews: 4
   }
 ];
 
